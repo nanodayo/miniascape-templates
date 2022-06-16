418141199040b00dc68e99a5ba978d773bb2a7da
Masatake YAMATO <yamato@redhat.com>
2022-06-16 23:12:10 +0900

    change: [rhui4] use .ssh/id_rsa_rhua when adding a cds or a haproxy
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- autoinstall.d/data/rhui/4/rhua/75_setup_haproxy.sh
+++ autoinstall.d/data/rhui/4/rhua/75_setup_haproxy.sh
@@ -6,10 +6,10 @@ source ${0%/*}/config.sh
 
 for lb in ${LB_SERVERS:?}; do
     test ${FORCE_ADD_LB:?} = 1 && \
-        rhui-manager --noninteractive haproxy add --hostname ${lb} --ssh_user root --keyfile_path /root/.ssh/id_rsa --force \
+        rhui-manager --noninteractive haproxy add --hostname ${lb} --ssh_user root --keyfile_path /root/.ssh/id_rsa_rhua --force \
     || (
         rhui-manager --noninteractive haproxy list --machine_readable | grep -E "hostname.: .${lb}" || \
-        rhui-manager --noninteractive haproxy add --hostname ${lb} --ssh_user root --keyfile_path /root/.ssh/id_rsa
+        rhui-manager --noninteractive haproxy add --hostname ${lb} --ssh_user root --keyfile_path /root/.ssh/id_rsa_rhua
     )
 done
 
